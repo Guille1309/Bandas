7b89c19e0b918f48bd7c5932bf39bba312864ccf
julimartiarenaa <jmartiarena@udesa.edu.ar>
2024-03-25 15:05:16 -0300

    arreglando un error

--- controllers/bandasController.js
+++ controllers/bandasController.js
@@ -7,7 +7,7 @@ const bandasController = {
     },
     detalleBanda: function (req, res) {
         let idRecibido = req.params.id;
-        let bandaId 
+        let bandaId;
         for (let i = 0; i < bandas.length; i++) {
             let idBanda = bandas[i].id;
             if (idRecibido == idBanda){
@@ -18,21 +18,16 @@ const bandasController = {
     },
     generoBanda: function (req, res) {
         let idGenero = req.params.id;
-        let generos = {} 
+        let generos = [];
         for (let i = 0; i < bandas.length; i++) {
             if (bandas[i].genero != generos){
-                generos.bandas[i].genero;
+                generos.push(bandas[i]);
             }
             else {
-                break
+                continue
             }
         };
-        for (let i = 0; i < bandas.length; i++) {
-            if (idObtenido == bandas[i].genero){
-                generos.idGenero = bandas[i];
-            }
-        };
-
+        
         res.render('porGenero', {generos: generos})
 }
 }
